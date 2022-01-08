374d784084debeab7cbcfa4162cea92b61e2c014
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-08 07:33:31 +0500

    graphql fetch with react
    problem was in gql_config.js dile......replace url to uri

--- src/Student.js
+++ src/Student.js
@@ -4,24 +4,48 @@ const GET_STUDENTS = gql`
     query {
       students {
         id,
-        name
-      }
+        name,
+        email,
+      age
+          }
     }
   `;
 
 function Student() {
 
-    const { loading, error, data } = useQuery(GET_STUDENTS);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error </p>;
-    const {students}=data;
-    
-    return (
-        <div>
-            <h1>hello data</h1>
-            console.log(data)
-        </div>
-    )
+  const { loading, error, data } = useQuery(GET_STUDENTS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error </p>;
+  const { students } = data;
+
+  return (
+    <div>
+      <h1>hello data</h1>
+      {console.log(students)}
+      <table border="3" width={500}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(dat => {
+            return (
+              <tr key={dat.id}>
+                <td>{dat.name}</td>
+                <td>{dat.email}</td>
+                <td>{dat.age}</td>
+              </tr>
+            );
+          })}
+
+        </tbody>
+      </table>
+    </div>
+  );
 
 }
 
