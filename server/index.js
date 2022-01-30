850c5dc8423904f0a09dd4516c6885567764692e
sulemansarwar10 <sulemansarwar10@gmail.com>
2022-01-30 23:12:10 +0500

    create mutation in graphql and subscription is not working yet

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
+//const { PubSub } = require('graphql-subscriptions');
 const { ApolloServer, gql } = require('apollo-server');
+
+//const pubsub = new PubSub();
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -20,6 +23,14 @@ const typeDefs = gql`
   type Query {
     students: [Student]
   }
+  type Mutation {
+    addStudent(name: String!, email: String!, age: Int!): Student!
+    updateUser(id: Int!, name: String, email: String, age: Int): Student!
+  }
+#  type Subscription {
+#   userCreated: Student!
+#  }
+
 `;
 const student = [
     {
@@ -41,12 +52,44 @@ const student = [
         age: 19,
     },
 ];
+const NEW_USER = "NEW_USER"
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
         students: () => student,
     },
+    Mutation: {
+        addStudent: (parent, { name, email, age }) => {
+            const id = student.length + 1;
+            const newstd = {
+                id,
+                name,
+                email,
+                age
+            }
+         //   pubsub.publish(NEW_USER, { userCreated:newstd });
+            student.push(newstd)
+            return student[student.findIndex(obj => obj.id === id)];
+        },
+        updateUser: (parent, { id, name, email, age }) => {
+            const objind = student.findIndex((obj => obj.id == id))
+            
+            if (name) { student[objind].name = name; }
+            if (email) { student[objind].email = email; }
+            if (age) { student[objind].age = age; }
+
+            return student[objind];
+        }
+    },
+    // Subscription: {
+    //     userCreated: {
+    //         subscribe: () => pubsub.asyncIterator(NEW_USER)
+    //     }
+    // },
+
+
+
 };
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
